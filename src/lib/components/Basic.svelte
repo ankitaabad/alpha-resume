f2f7d7870f8f16ab58b6a51350d7af0dd911b783
ankit <ankitaabad.1@gmail.com>
2023-03-24 11:55:56 +0530

    make fonts dynamic

--- src/lib/components/Basic.svelte
+++ src/lib/components/Basic.svelte
@@ -11,16 +11,14 @@
 		console.log({ avatar });
 	});
 	$: console.log({ avatar });
+	$: basic_data = $preview_data?.Basic?.data[0];
+	$: social_profiles = $preview_data?.['Social Profile']?.data;
 </script>
 
 <div class="mx-5">
 	<!-- <h1 class="text-2xl font-semibold ">{$preview_data?.['Social Profile']?.data[0]?.Network}</h1> -->
 
-	<div
-		class={$preview_data?.Basic?.data[0]?.Name
-			? 'flex pb-6 gap-6 border-b border-solid border-gray-300'
-			: 'flex'}
-	>
+	<div class={basic_data.Name ? 'flex pb-6 gap-6 border-b border-solid border-gray-300' : 'flex'}>
 		<div
 			class="aspect-square w-32 h-32 bg-transparent flex items-center justify-center"
 			class:hidden={avatar?.image === undefined}
@@ -29,31 +27,37 @@
 		</div>
 		<div class="profile-details w-full flex justify-between">
 			<div>
-				<h1 class="text-2xl font-semibold ">{$preview_data?.Basic?.data[0]?.Name}</h1>
-				<div class=" mb-4" class:hidden={$preview_data?.Basic?.data[0].Designation === ''}>
-					{$preview_data?.Basic?.data[0].Designation}
+				<h1 class="text-2xl font-semibold ">{basic_data?.Name}</h1>
+				<div class=" mb-4" class:hidden={basic_data?.Designation === ''}>
+					{basic_data?.Designation}
 				</div>
 			</div>
 			<div class="flex gap-3 flex-col">
 				<div class="flex flex-col ">
-					<p class="w-full" class:hidden={$preview_data?.Basic?.data[0].Email == ''}>
-						<a href={$preview_data?.Basic?.data[0].Email} class="text-gray-500"
-							><svelte:component class="inline" this ={get_social_icon('email')}/>
-							{$preview_data?.Basic?.data[0].Email}</a
+					<p class="w-full" class:hidden={basic_data?.Email == ''}>
+						<a href={basic_data?.Email} class="text-gray-500"
+							><svelte:component this={get_social_icon('email')} class="inline" />
+							{basic_data?.Email}</a
 						>
 					</p>
 					<p class="w-full" class:hidden={$preview_data?.Basic?.data[0].Phone == ''}>
 						<a href="/" class="text-gray-500"
-							><svelte:component class="inline" this ={get_social_icon('phone')}/>{$preview_data?.Basic?.data[0].Phone}</a
+							><svelte:component this={get_social_icon('phone')} class="inline" />{$preview_data
+								?.Basic?.data[0].Phone}</a
 						>
 					</p>
-					<p class="w-full" class:hidden={$preview_data?.Basic?.data[0].Location == ''}>
-            <svelte:component class="inline text-gray-500" this ={get_social_icon('location')}/>	
-						<span class="text-gray-500">{$preview_data?.Basic?.data[0].Location}</span>
+					<p class="w-full" class:hidden={basic_data?.Location == ''}>
+						<svelte:component this={get_social_icon('location')} class="inline text-gray-500" />
+						<span class="text-gray-500">{basic_data?.Location}</span>
 					</p>
-          <p class="w-full" class:hidden={$preview_data?.['Social Profile']?.data[0].Username == ''}>
-            <svelte:component class="inline text-gray-500" this ={get_social_icon('linkedin')}/>	
-						<a href = {$preview_data?.['Social Profile']?.data[0].URL} class="text-gray-500">{$preview_data?.['Social Profile']?.data[0].Username}</a>
+					<p
+						class="w-full"
+						class:hidden={$preview_data?.['Social Profile']?.data[0].Username == ''}
+					>
+						<svelte:component this={get_social_icon('linkedin')} class="inline text-gray-500" />
+						<a href={$preview_data?.['Social Profile']?.data[0].URL} class="text-gray-500"
+							>{$preview_data?.['Social Profile']?.data[0].Username}</a
+						>
 					</p>
 				</div>
 			</div>
@@ -62,19 +66,10 @@
 	<!-- container -->
 	<div class="flex flex-col pb-5  ">
 		<!-- profile -->
-		<div
-			class={$preview_data?.Basic?.data[0].Summary
-				? 'profile py-6 border-b border-solid border-gray-300'
-				: ''}
-		>
-			<h2
-				class="text-xl font-semibold mb-2"
-				class:hidden={$preview_data?.Basic?.data[0].Summary === ''}
-			>
-				About Me
-			</h2>
+		<div class={basic_data?.Summary ? 'profile py-6 border-b border-solid border-gray-300' : ''}>
+			<h2 class="text-xl font-semibold mb-2" class:hidden={basic_data?.Summary === ''}>About Me</h2>
 			<p class="text-gray-500 text-justify">
-				{$preview_data?.Basic?.data[0].Summary}
+				{basic_data?.Summary}
 			</p>
 		</div>
 	</div>
